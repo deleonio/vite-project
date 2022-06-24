d4258f693eaba4e5faeca0cbaf7ba2d62496232d
Martin Oppitz <martin.oppitz@itzbund.de>
2022-06-24 12:21:52 +0200

    fix esm integration

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
-import { defineCustomElements } from '@kolibri/lib/dist/loader'
-import { register } from '@kolibri/core'
-import { BMF } from '@kolibri/themes'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import { KolBadge, KolKolibri } from '@kolibri/react'
+import { defineCustomElements, KolKolibri, register } from "@kolibri/react";
+import { BMF } from "@kolibri/themes";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-register(BMF, defineCustomElements).then(()=> {
-  ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-      <KolKolibri></KolKolibri>
-      <App />
-    </React.StrictMode>
-  )
-}).catch(console.warn);
+register(BMF, defineCustomElements)
+  .then(() => {
+    ReactDOM.createRoot(document.getElementById("root")!).render(
+      <React.StrictMode>
+        <KolKolibri></KolKolibri>
+        <App />
+      </React.StrictMode>
+    );
+  })
+  .catch(console.warn);
